8b54a8623101ffce64de618a7173a5446716e081
Joanna M. Rives <joanna.rives@gmail.com>
2018-07-21 13:35:44 -0700

    Helper file for exercises 40 & 40a.

---
+++ mystuff.py
@@ -0,0 +1,6 @@
+#! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
+# -*- coding: utf-8 -*-
+# helper file for ex40a_mc.py
+def apple():
+    print("I AM APPLES!")
+    tangerine = "Living reflection of a dream"
